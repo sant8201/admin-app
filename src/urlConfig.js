cabb0d5fdd248f350d77a380978e47d8a57b8a4e
Jorge Luis dos Santos <71775223+sant8201@users.noreply.github.com>
2021-03-27 22:18:10 +0300

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,8 +1,9 @@
-//const baseUrl = location.hostname === "localhost" 
-//? "http://localhost:2000" 
-//: "https://pragas-rest-server.herokuapp.com";
+const baseUrl = 
+      location.hostname === "localhost" 
+      ? "http://localhost:2000" 
+      : "https://pragas-rest-server.herokuapp.com";
 
-const baseUrl = process.env.API || "https://pragas-rest-server.herokuapp.com";
+//const baseUrl = process.env.API || "https://pragas-rest-server.herokuapp.com";
 //const baseUrl = "http://localhost:2000";
 
 export const api = `${baseUrl}/api`;
